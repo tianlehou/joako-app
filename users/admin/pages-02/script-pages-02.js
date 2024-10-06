bb3e6a80269d8dbe5e8ea3c8115780a544ab0365
tianlehou <86812844+tianlehou@users.noreply.github.com>
2024-10-05 22:58:31 -0500

    Add files via upload

---
+++ users/admin/pages-02/script-pages-02.js
@@ -0,0 +1,95 @@
+import { ref, onValue } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
+import { database } from "../../../environment/firebaseConfig.js";
+
+import "./modules/newRegister.js";
+import { checkAuth } from '../../../modules/accessControl/authCheck.js';
+import { checkUserAccess } from "../../../modules/accessControl/roleAccessControl.js";
+
+import "./modules/downloadToExcel.js";
+import { deleteRow } from "./modules/deleteRow.js";
+import { addEditEventListeners } from "./modules/editRow.js";
+import { handleFileUpload } from '../../../modules/Excel/uploadExcelHandler.js';
+
+import { initializeSearch } from "./modules/searchFunction.js";
+import { initScrollButtons } from "../modules/scrollButtons.js";
+import { includeHTML } from '../components/includeHTML/includeHTML.js';
+import { updateSelectElements } from './modules/updateSelectElements.js';
+import { updateTotalSums } from './modules/sumColumns.js';
+import { getDaysInMonth, getMonthAndYearFromURL, generateCalendarDays } from './modules/calendarUtils.js';
+
+// Constantes y variables de estado
+const tabla = document.getElementById("contenidoTabla");
+
+// Lee la variable collection desde el HTML
+export const collection = (() => {
+    const scriptTag = document.querySelector('script[data-collection]');
+    if (scriptTag) {
+        return scriptTag.getAttribute('data-collection');
+    }
+})();
+
+if (!collection) {
+    console.error('La variable collection está vacía.');
+}
+
+export function mostrarDatos() {
+    const { month, year } = getMonthAndYearFromURL();
+
+    if (!collection) {
+        console.error('La ruta de la colección es inválida.');
+        return;
+    }
+
+    onValue(ref(database, collection), (snapshot) => {
+        tabla.innerHTML = "";
+
+        const data = [];
+        snapshot.forEach((childSnapshot) => {
+            const user = childSnapshot.val();
+            data.push({ id: childSnapshot.key, ...user });
+        });
+
+        data.sort((a, b) => a.nombre.localeCompare(b.nombre));
+
+        let filaNumero = 1;
+        data.forEach((user) => {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                <td class="text-center">${filaNumero++}</td>
+                <td class="text-center">${user.nombre}</td>
+                ${generateCalendarDays(month, year, user)}
+                <td class="display-flex-center">
+                    <button class="btn btn-primary mg-05em edit-user-button" data-id="${user.id}">
+                        <i class="bi bi-pencil"></i>
+                    </button>
+                    <button class="btn btn-danger mg-05em delete-user-button" data-id="${user.id}">
+                        <i class="bi bi-eraser-fill"></i>
+                    </button>
+                </td>
+                <td class="text-center">
+                    <span class="${!user.userId ? 'invisible-value' : ''}">${user.userId || ''}</span>
+                </td>
+            `;
+            tabla.appendChild(row);
+        });
+
+        addEditEventListeners(database, collection); // Asegúrate de que esto esté aquí
+        deleteRow(database, collection);
+        updateSelectElements(database, collection);
+        updateTotalSums(tabla, Array.from({ length: getDaysInMonth(month, year) }, (_, i) => i + 2));
+    });
+}
+
+// Inicializa la tabla y eventos al cargar el documento
+document.addEventListener('DOMContentLoaded', () => {
+    checkAuth();
+    checkUserAccess();
+    
+    mostrarDatos();
+    includeHTML();
+    initializeSearch(tabla);
+    initScrollButtons(tabla);
+    handleFileUpload();
+});
+
+console.log(database);
---
+++ users/admin/pages/script-pages-01.js
@@ -0,0 +1,103 @@
+import { ref, onValue, update } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
+import { database } from "../../../environment/firebaseConfig.js";
+
+import "../auth/signup_Form.js";
+import { checkAuth } from '../../../modules/accessControl/authCheck.js';
+import { checkUserAccess } from "../../../modules/accessControl/roleAccessControl.js";
+
+import { deleteRow } from "../modules/tabla/deleteRow.js";
+import { addEditEventListeners } from "./modules/editRow.js";
+import "../../../modules/excel/downloadToExcel-biblioteca.js";
+
+import { initializeSearch } from "./modules/searchFunction.js";
+import { includeHTML } from '../components/includeHTML/includeHTML.js';
+import { changeEstadoSelectEvent, changeRoleSelectEvent } from "../modules/tabla/changeSelectEvent.js";
+import { filterDataByRole } from "../../../modules/tabla/filterData/filterDataByRole.js"; // Importar la función de filtrado
+
+// Constantes y variables de estado
+const tabla = document.getElementById("contenidoTabla");
+
+export function mostrarDatos() {
+  onValue(ref(database, collection), async (snapshot) => {
+    tabla.innerHTML = "";
+
+    const data = [];
+    snapshot.forEach((childSnapshot) => {
+      data.push({ id: childSnapshot.key, ...childSnapshot.val() });
+    });
+
+    // Filtrar los datos según el rol del usuario
+    const filteredData = await filterDataByRole(data);
+
+    // Ordenar alfabéticamente los datos filtrados
+    filteredData.sort((a, b) => a.nombre.localeCompare(b.nombre));
+
+    let filaNumero = 1;
+
+    for (let i = 0; i < filteredData.length; i++) {
+      const user = filteredData[i];
+      const row = `
+            <tr>
+              <td class="text-center">${filaNumero++}</td>
+              <td class="text-center">${user.unidad}</td>
+              <td class="text-center">${user.placa}</td>
+              <td class="text-center">${user.nombre}</td>
+              <td class="text-center">${user.cedula}</td>
+              <td class="text-center">
+                <a href="https://wa.me/${user.whatsapp}" target="_blank">
+                  ${user.whatsapp}
+                </a>
+              </td>
+              <td class="text-center estado-col">
+                <div class="flex-container">
+                  <span>${user.estado}</span>
+                  <select class="form-select estado-select" data-id="${user.id}">
+                    <option value="Ninguno" ${user.estado === "Ninguno" ? "selected" : ""}>Ninguno</option>
+                    <option value="Activo" ${user.estado === "Activo" ? "selected" : ""}>Activo</option>
+                    <option value="Sin carro" ${user.estado === "Sin carro" ? "selected" : ""}>Sin carro</option>
+                    <option value="Suspendido" ${user.estado === "Suspendido" ? "selected" : ""}>Suspendido</option>
+                    <option value="Expulsado" ${user.estado === "Expulsado" ? "selected" : ""}>Expulsado</option>
+                  </select>
+                </div>
+              </td>
+              <td class="text-center role-col">
+                <div class="flex-container">
+                  <span>${user.role}</span>
+                  <select class="form-select role-select" data-id="${user.id}">
+                    <option value="" ${user.role === "" ? "selected" : ""}></option>
+                    <option value="Administrador" ${user.role === "Administrador" ? "selected" : ""}>Administrador</option>
+                    <option value="Cobrador" ${user.role === "Cobrador" ? "selected" : ""}>Cobrador</option>
+                    <option value="Propietario" ${user.role === "Propietario" ? "selected" : ""}>Propietario</option>
+                    <option value="Conductor" ${user.role === "Conductor" ? "selected" : ""}>Conductor</option>
+                    <option value="Secretario" ${user.role === "Secretario" ? "selected" : ""}>Secretario</option>
+                  </select>
+                </div>
+              </td>
+              <td>
+                <button class="btn btn-primary edit-user-button" data-id="${user.id}"><i class="bi bi-highlighter"></i></button>
+                <button class="btn btn-danger delete-user-button" data-id="${user.id}"><i class="bi bi-eraser-fill"></i></button>
+              </td>
+              <td class="text-center">${user.email}</td>
+              <td class="text-center">${user.userId}</td>
+            </tr>
+          `;
+      tabla.innerHTML += row;
+    }
+    deleteRow(database, collection);
+    addEditEventListeners(database, collection);
+  });
+}
+
+// Inicializa la tabla y eventos al cargar el documento
+document.addEventListener('DOMContentLoaded', () => {
+  checkAuth();
+  checkUserAccess();
+  
+  includeHTML();
+  mostrarDatos();
+  initializeSearch(tabla);
+  changeRoleSelectEvent(tabla, database, collection);
+  changeEstadoSelectEvent(tabla, database, collection);
+});
+
+console.log(database);
